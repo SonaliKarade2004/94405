e6d266306f41a349d6c802144f50d8b1b931b7a4
SonaliKarade <karadesonali2408@gmail.com>
2025-12-10 23:05:40 +0530

    Initialize changes

---
+++ geometry.py
@@ -0,0 +1,7 @@
+print("Welcome to first task of git")
+
+def area(a):
+    print("Area of square :",a*a)
+def per(b) :
+    print("Perimeter of square : ",4*b)
+
---
+++ main.py
@@ -0,0 +1,6 @@
+from geometry import area 
+from geometry import per
+
+num1 = int(input("Enter radius : "))
+area(num1)
+per(num1)
